47c3b09df2d96a482ab67691060cdeaf6dcdec9a
Sebastian Lopez-Cot <sebastian.lopez.cot@gmail.com>
2023-03-03 08:00:58 -0500

    Fixes

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -52,6 +52,10 @@ public class Robot extends TimedRobot {
     // autonomous chooser on the dashboard.
     camera1 = CameraServer.startAutomaticCapture(0);
     camera2 = CameraServer.startAutomaticCapture(1);
+    camera1.setResolution(320, 240);
+    camera2.setResolution(320, 240);
+    camera1.setFPS(15);
+    camera2.setFPS(15);
     server = CameraServer.getServer();
 
     camera1.setConnectionStrategy(ConnectionStrategy.kKeepOpen);
